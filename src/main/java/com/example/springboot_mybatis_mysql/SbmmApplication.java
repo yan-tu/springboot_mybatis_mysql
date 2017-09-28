1b171fba27ed7791d250db212ab688e7618b39bf
yantu <yan_tu@qq.com>
2017-09-28 18:32:30 +0800

    新建项目

---
+++ src/main/java/com/example/springboot_mybatis_mysql/SbmmApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_mybatis_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmmApplication.class, args);
+	}
+}
