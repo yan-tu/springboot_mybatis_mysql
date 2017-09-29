054e1be964baf5fa3ad64176f49ac98afffa8713
yantu <yan_tu@qq.com>
2017-09-29 08:47:04 +0800

    提交测试

--- src/main/java/com/example/springboot_mybatis_mysql/SbmmApplication.java
+++ src/main/java/com/example/springboot_mybatis_mysql/SbmmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbmmApplication {
-
 	public static void main(String[] args) {
 		System.out.println();
 		SpringApplication.run(SbmmApplication.class, args);
